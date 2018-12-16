0c5d2c7d46909e084b2cbb0900eb671ab4d908ac
asktree <ask252@cornell.edu>
2018-12-15 20:35:43 -0500

    added logic

--- src/App.js
+++ src/App.js
@@ -29,11 +29,55 @@ var opmlstr =
 
 var honk = x2js.xml2js(opmlstr);
 
+const parse_cost = str => {
+  var n1 = str.indexOf("| cost: ") + 8;
+  if (n1 == -1) {throw "'| cost: ' not found in task string";}
+  var n2 = str.slice(n1).indexOf("|")
+  if (n2 == -1) {n2 = str.length;}
+  var cost = str.substring(n1,n2);
+  var cost = parseFloat(cost);
+  return cost;
+}
+
+const parse_reward = str => {
+  var n1 = str.indexOf("| reward: ") + 10;
+  if (n1 == -1) {throw "'| reward: ' not found in task string";}
+  var n2 = str.slice(n1).indexOf("|")
+  if (n2 == -1) {n2 = str.length;}
+  var cost = str.substring(n1,n2);
+  var cost = parseFloat(cost);
+  return cost;
+}
+
+const convert2api = dat => {
+  let newDat = JSON.parse(JSON.stringify(dat));
+  var goals = newDat["opml"]["body"]["outline"]["outline"];
+  console.log(goals);
+  for (var goal of goals.entries()) {
+    console.log(goal)
+    var reward = parse_reward(goal[1]["_text"]);
+    console.log(reward)
+    var total_costs = 0
+    for (let task of goal[1]["outline"].entries()){
+      console.log(task);
+      console.log(task[1]["_text"])
+      total_costs += parse_cost(task[1]["_text"])
+      console.log(total_costs)
+    }
+    for (let task of goal[1]["outline"].entries()){
+      let portion = parse_cost(task[1]["_text"]) / total_costs;
+      let task_reward = reward * portion;
+      goals[goal[0]]["outline"][task[0]]["_text"] += " | reward: " + task_reward.toString();
+    }
+  }
+  return newDat;
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ReactJson src={honk} />
+        <ReactJson src={convert2api(honk)} />
       </div>
     );
   }
